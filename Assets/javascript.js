963b589315071f53ec88f629644296edb89ec1b9
Julian Pluas <julianpluas@gmail>
2020-12-29 16:58:11 -0800

    Erased old code and wrote some starter code from scratch

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -1,5 +1,9 @@
-$("#forecastbutton").on("click",function(event){
-    event.preventDefault();
-    var city = $("#userinput").val()
-    console.log(city)
-})
+var apiKey = "48af17a7060c2205e40c1b9e5e56df19";
+
+$.ajax(
+  "http://api.openweathermap.org/data/2.5/weather?q=New%20York&units=imperial&appid=48af17a7060c2205e40c1b9e5e56df19"
+).then(function (data) {
+  console.log(data);
+  $("#city").text(data.name);
+});
+console.log("hello");
