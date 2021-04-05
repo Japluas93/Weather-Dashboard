0fdac8a9704f822860a65ef5af16292e6d01ca55
Julian Pluas <julianpluas@gmail>
2021-04-05 13:51:06 -0700

    Changed function name

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -39,91 +39,91 @@ $("#forecastbutton").on("click", function (event) {
   todayWeather(currentCity);
 });
 
-// function getWeatherFunc(data) {
-//   console.log(data);
-//   $("#city").text("City: " + data.city.name);
-//   $("#currenttemp").text("Temperature: " + data.list.main.temp + "°F");
-//   $("#currentwind").text("Wind Speed: " + data.wind.speed + " MPH");
-//   $("#currenthumidity").text("Humidity: " + data.main.humidity + "%");
-//   // Ajax call for the 5 day forecast
-//   $.ajax(
-//     "https://api.openweathermap.org/data/2.5/forecast?q=" +
-//       data +
-//       "lat=" +
-//       data.coord.lat +
-//       "&lon=" +
-//       data.coord.lon +
-//       "&units=imperial&appid=" +
-//       apiKey
-//   ).then(function (forecastData) {
-//     // console.log(forecastData);
-//     // console.log(forecastData.daily[0].dt);
-//     $("#currentuv").text("UV: " + forecastData.current.uvi);
+function fiveDayForecast(data) {
+  console.log(data);
+  $("#city").text("City: " + data.city.name);
+  $("#currenttemp").text("Temperature: " + data.list.main.temp + "°F");
+  $("#currentwind").text("Wind Speed: " + data.wind.speed + " MPH");
+  $("#currenthumidity").text("Humidity: " + data.main.humidity + "%");
+  // Ajax call for the 5 day forecast
+  $.ajax(
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      data +
+      "lat=" +
+      data.coord.lat +
+      "&lon=" +
+      data.coord.lon +
+      "&units=imperial&appid=" +
+      apiKey
+  ).then(function (forecastData) {
+    console.log(forecastData);
+    console.log(forecastData.daily[0].dt);
+    $("#currentuv").text("UV: " + forecastData.current.uvi);
 
-//     $("#dayone").html(`<div class="card">
-//     <div class="card-body">
-//       <p id="date" class="card-text">${new Date(
-//         forecastData.daily[0].dt
-//       ).toDateString()} </p>
-//       <p id="currenttemp" class="card-text">Temp: ${
-//         forecastData.daily[1].temp.day
-//       } F</p>
-//       <p id="currenthumidity" class="card-text">Humidity: ${
-//         forecastData.daily[1].humidity
-//       }% </p>
-//     </div>
-//   </div>`);
-//     $("#daytwo").html(`<div class="card">
-//     <div class="card-body">
-//     <p id="date" class="card-text">${new Date(
-//       forecastData.daily[1].dt
-//     ).toDateString()} </p>
-//       <p id="currenttemp" class="card-text">Temp: ${
-//         forecastData.daily[2].temp.day
-//       } F</p>
-//       <p id="currenthumidity" class="card-text">Humidity: ${
-//         forecastData.daily[2].humidity
-//       }% </p>
-//     </div>
-//   </div>`);
-//     $("#daythree").html(`<div class="card">
-//     <div class="card-body">
-//     <p id="date" class="card-text">${new Date(
-//       forecastData.daily[2].dt
-//     ).toDateString()} </p>
-//       <p id="currenttemp" class="card-text">Temp: ${
-//         forecastData.daily[3].temp.day
-//       } F</p>
-//       <p id="currenthumidity" class="card-text">Humidity: ${
-//         forecastData.daily[3].humidity
-//       }% </p>
-//     </div>
-//   </div>`);
-//     $("#dayfour").html(`<div class="card">
-//     <div class="card-body">
-//     <p id="date" class="card-text">${new Date(
-//       forecastData.daily[4].dt
-//     ).toDateString()} </p>
-//       <p id="currenttemp" class="card-text">Temp: ${
-//         forecastData.daily[4].temp.day
-//       } F</p>
-//       <p id="currenthumidity" class="card-text">Humidity: ${
-//         forecastData.daily[4].humidity
-//       }% </p>
-//     </div>
-//   </div>`);
-//     $("#dayfive").html(`<div class="card">
-//     <div class="card-body">
-//     <p id="date" class="card-text">${new Date(
-//       forecastData.daily[5].dt
-//     ).toDateString()} </p>
-//       <p id="currenttemp" class="card-text">Temp: ${
-//         forecastData.daily[5].temp.day
-//       } F</p>
-//       <p id="currenthumidity" class="card-text">Humidity: ${
-//         forecastData.daily[5].humidity
-//       }% </p>
-//     </div>
-//   </div>`);
-//   });
-// }
+    $("#dayone").html(`<div class="card">
+    <div class="card-body">
+      <p id="date" class="card-text">${new Date(
+        forecastData.daily[0].dt
+      ).toDateString()} </p>
+      <p id="currenttemp" class="card-text">Temp: ${
+        forecastData.daily[1].temp.day
+      } F</p>
+      <p id="currenthumidity" class="card-text">Humidity: ${
+        forecastData.daily[1].humidity
+      }% </p>
+    </div>
+  </div>`);
+    $("#daytwo").html(`<div class="card">
+    <div class="card-body">
+    <p id="date" class="card-text">${new Date(
+      forecastData.daily[1].dt
+    ).toDateString()} </p>
+      <p id="currenttemp" class="card-text">Temp: ${
+        forecastData.daily[2].temp.day
+      } F</p>
+      <p id="currenthumidity" class="card-text">Humidity: ${
+        forecastData.daily[2].humidity
+      }% </p>
+    </div>
+  </div>`);
+    $("#daythree").html(`<div class="card">
+    <div class="card-body">
+    <p id="date" class="card-text">${new Date(
+      forecastData.daily[2].dt
+    ).toDateString()} </p>
+      <p id="currenttemp" class="card-text">Temp: ${
+        forecastData.daily[3].temp.day
+      } F</p>
+      <p id="currenthumidity" class="card-text">Humidity: ${
+        forecastData.daily[3].humidity
+      }% </p>
+    </div>
+  </div>`);
+    $("#dayfour").html(`<div class="card">
+    <div class="card-body">
+    <p id="date" class="card-text">${new Date(
+      forecastData.daily[4].dt
+    ).toDateString()} </p>
+      <p id="currenttemp" class="card-text">Temp: ${
+        forecastData.daily[4].temp.day
+      } F</p>
+      <p id="currenthumidity" class="card-text">Humidity: ${
+        forecastData.daily[4].humidity
+      }% </p>
+    </div>
+  </div>`);
+    $("#dayfive").html(`<div class="card">
+    <div class="card-body">
+    <p id="date" class="card-text">${new Date(
+      forecastData.daily[5].dt
+    ).toDateString()} </p>
+      <p id="currenttemp" class="card-text">Temp: ${
+        forecastData.daily[5].temp.day
+      } F</p>
+      <p id="currenthumidity" class="card-text">Humidity: ${
+        forecastData.daily[5].humidity
+      }% </p>
+    </div>
+  </div>`);
+  });
+}
